d73d362265c52f9a2c943ac16f4df77ee8cffcc2
Fred Medlin <fred@medl.in>
2015-03-06 09:54:15 -0500

    Handle errors in reverse order to make top field focusable

--- app/src/main/java/twotoasters/org/tika/sample/FormPresenter.java
+++ app/src/main/java/twotoasters/org/tika/sample/FormPresenter.java
@@ -2,6 +2,7 @@ package twotoasters.org.tika.sample;
 
 import com.squareup.otto.Subscribe;
 
+import java.util.Collections;
 import java.util.List;
 
 import twotoasters.org.tika.FieldError;
@@ -17,10 +18,22 @@ public class FormPresenter {
         this.view = view;
     }
 
+    /**
+     * Validate and display errors.
+     *
+     * Handles errors in reverse order so that View.setError() focus is
+     * returned to the "top" field in the list.
+     *
+     * @param event Contains the form/fields to validate
+     */
     @Subscribe
     @SuppressWarnings("unused")
     public void onFormSubmit(FormSubmitEvent event) {
         List<FieldError> errors = model.validate(view.getActivity(), event.getForm());
+        if (errors.size() > 1) {
+            Collections.reverse(errors);
+        }
+
         if (errors.isEmpty()) {
             // Do the success case
         } else {
@@ -29,4 +42,5 @@ public class FormPresenter {
             }
         }
     }
+
 }
